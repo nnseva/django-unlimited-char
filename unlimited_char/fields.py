a55ff6f552e5ef415b124e8200538395281ad765
Vsevolod Novikov <nnseva@gmail.com>
2023-02-14 23:11:35 +0300

    Upgrading to the django4

--- unlimited_char/fields.py
+++ unlimited_char/fields.py
@@ -1,6 +1,6 @@
 """Field definitions"""
 from django.db import models, connection
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core import validators
 
 
